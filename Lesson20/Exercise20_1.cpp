f76d63f96e5d65bedd6ce86ced2f479f4e5ad9e0
Neill Henning <neillhenning@gmail.com>
2017-04-04 23:10:58 +0200

    Finished Exercise 20.1

--- Lesson20/Exercise20_1.cpp
+++ Lesson20/Exercise20_1.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void drawFrame(int n, int m)
+{
+    cout << endl;
+    for(int i = 1; i <= m; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == m)
+                cout << "* ";
+            else if (j == 1 || j == n)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    int width, height;
+
+    //Get width and height values of the rectangle
+    cout << "Please enter the width and height of the rectangle separated by a space: ";
+    cin >> width >> height;
+
+    //draw the frame
+    drawFrame(width, height);
 
     return 0;
 }
