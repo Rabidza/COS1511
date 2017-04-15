3fe8e254ac781d4ab0062238c375fd3fc4ed5ff7
Neill Henning <neillhenning@gmail.com>
2017-04-15 19:51:21 +0200

    Fixed exercise 20

--- Lesson20/Exercise20_2.cpp
+++ Lesson20/Exercise20_2.cpp
@@ -1,8 +1,31 @@
+//Draws a tree
 #include <iostream>
 using namespace std;
 
-int main()
+void displayOneLine(int spacesP, int asterisksP)
 {
+    for (int i = 1; i <= spacesP; i++)
+        cout << " ";
+    for (int i = 1; i <= asterisksP; i++)
+        cout << "* ";
+    cout << endl;
+}
+
+int main( )
+{
+    int size;
+
+    cout << "Enter the size of the tree: ";
+    cin >> size;
+
+    //the tree top
+    cout << endl;
+    for (int i = 1; i <= size; i++)
+    displayOneLine(size - i,i);
+
+    //the trunk
+    displayOneLine(size - 1,1);
+    displayOneLine(size - 1,1);
 
     return 0;
 }
--- Lesson20/Exercise20_3.cpp
+++ Lesson20/Exercise20_3.cpp
@@ -7,29 +7,40 @@ void displayRow(int n, char c)
         cout << c;
     cout << endl;
 }
+
 int main( )
 {
-    //TODO: change average to a floating point value
-    int value, total, many, average;
+    int value, total, many, averageInt;
+    float average;
     total = 0;
     many = 0;
 
-    //TODO: Prevent user from entering incorrect values
-
     cout << "Enter the values (negative to end):" << endl;
     cin >> value;
-    while (value > 0)
+
+    while (value >= 0)
     {
+        if (value > 80)
+            do
+            {
+                cout << "Value cannot be more than 80. Enter again: ";
+                cin >> value;
+            } while (value > 80);
+
         total += value;
         many++;
         displayRow(value, '*');
         cin >> value;
     }
-
-    //TODO: Apply rounding to the floating point average before calling displayRow to display the appropriate number of plus signs.
-    average = total/many;
-    cout << "The average is " << average << endl;
-    displayRow(average, '+');
+    if (many > 0)
+    {
+        average = float(total)/many;
+        cout << "The average is " << average << endl;
+        averageInt = int(average + 0.5);
+        displayRow(averageInt, '+');
+    }
+    else
+        cout << "There were no values entered." << endl;
 
     return 0;
 }
